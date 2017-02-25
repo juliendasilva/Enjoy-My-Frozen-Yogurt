efb54f009555a9a15c88095071e8e001da2c9fc6
julien da silva <julien@julien.local>
2017-02-25 18:23:09 +0100

    [index:html] add svg for background

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ const gulp      = require('gulp'),
 gulp.task('html', () => {
   return gulp.src(path.join(__dirname, "app/index.html"))
     .pipe(gulp.dest(path.join(__dirname, 'dist/')))
-})
+});
 
 /* ------ Transpilation es6 to es5. ----- */
 gulp.task('es6:dev', () => {
@@ -19,7 +19,7 @@ gulp.task('es6:dev', () => {
       presets: ['es2015']
     }))
     .pipe(gulp.dest(path.join(__dirname, "dist/js/")))
-})
+});
 
 gulp.task('es6:prod', () => {
   return gulp.src(path.join(__dirname, 'app/js/app.js'))
@@ -29,14 +29,14 @@ gulp.task('es6:prod', () => {
     // minify
     .pipe(uglifyJs())
     .pipe(gulp.dest(path.join(__dirname, "dist/js/")))
-})
+});
 
 /* ------ Convert scss to regular css. ----- */
 gulp.task('scss:dev', () => {
   return gulp.src(path.join(__dirname, "app/scss/**/*.scss"))
     .pipe(sass())
     .pipe(gulp.dest('dist/css'))
-})
+});
 
 gulp.task('scss:prod', () => {
   return gulp.src(path.join(__dirname, "app/scss/**/*.scss"))
@@ -44,27 +44,27 @@ gulp.task('scss:prod', () => {
     // minify
     .pipe(uglifyCss())
     .pipe(gulp.dest('dist/css'))
-})
+});
 
 /* ------ Watch news in html, scss, js files. ----- */
 gulp.task('watch', () => {
-  gulp.watch(['./app/index.html'], ['html:dev']);
+  gulp.watch(['./app/index.html'], ['html']);
   gulp.watch(['./app/scss/**/*.scss'], ['scss:dev']);
   gulp.watch(['./app/js/**/*.js'], ['es6:dev']);
-})
+});
 
 /* ------ Build application. ----- */
 gulp.task('build:dev', ['html', 'es6:dev', 'scss:dev'], () => {
   // Make a build
   // Transpilation es6 to es5
   // Then ... adding some minification.
-})
+});
 
 gulp.task('build:prod', ['html', 'es6:prod', 'scss:prod'], () => {
   // Make a build
   // Transpilation es6 to es5
   // Then ... adding some minification.
-})
+});
 
 gulp.task('deploy', ['build:prod'], () => {
   gulp.src(path.join(__dirname, "/dist/**/*"))

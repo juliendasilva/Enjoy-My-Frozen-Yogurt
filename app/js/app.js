a139530aea9f0b8d0338d8b17e5eb8e8268881e1
julien da silva <julien@julien.local>
2017-03-04 21:49:06 +0100

    [jquey:js] ne plus utiliser jquery

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,24 @@
+(function() {
+  const durationByDefault = 250;
 
-$('a').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
-    }, 2000);
-    return false;
-});
+  function scrollTo(element, to, duration) {
+    if (duration <= 0) return;
+    const difference = to - element.scrollTop;
+    const perTick    = difference / duration * 2;
+
+    setTimeout(() => {
+      element.scrollTop = element.scrollTop + perTick;
+      if (element.scrollTop === to) return;
+      scrollTo(element, to, duration - 2);
+    }, 2);
+  }
+
+  const scrollToBlock2 = document.getElementById("scroll-to-block-2");
+
+  scrollToBlock2.addEventListener("click", (e) => {
+    e.preventDefault();
+    scrollTo(document.body, scrollToBlock2.offsetTop, durationByDefault);
+  });
+
+
+})();

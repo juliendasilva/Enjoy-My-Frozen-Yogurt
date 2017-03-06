76d78712ed38c9161a1e729385a7014d7f58ae0b
julien da silva <julien@julien.local>
2017-03-07 00:10:52 +0100

    Update 2017-03-06T23:10:45.549Z

--- js/changeImage.js
+++ js/changeImage.js
@@ -1,1 +1,29 @@
-"use strict";!function(){function e(){var e=document.getElementById("main-image-small"),n=document.getElementById("main-image-large"),t=document.body.clientWidth;t>=768?(e.style.display="none",n.style.display="block"):(n.style.display="none",e.style.display="block")}window.addEventListener("resize",function(n){e()}),window.onload=e}();
+'use strict';
+
+(function () {
+
+  //srset est fait pour charger la même image dans différentes résolutions
+  //srset n'est pas adapter dans notre cas car l'image change en fonction de la résolution
+
+  function changeMainImage() {
+    var imgInf768 = document.getElementById('main-image-small');
+    var imgSup768 = document.getElementById('main-image-large');
+
+    var width = document.body.clientWidth;
+    if (width >= 768) {
+      imgInf768.style.display = 'none';
+      imgSup768.style.display = 'block';
+    } else {
+      imgSup768.style.display = 'none';
+      imgInf768.style.display = 'block';
+    }
+  }
+
+  // changeMainImage();
+
+  window.addEventListener('resize', function (event) {
+    changeMainImage();
+  });
+
+  window.onload = changeMainImage;
+})();
